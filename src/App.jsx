c5d3f831c4223a8fd4a5e5d368edcfc1feafb7d5
Bhatt-Om <122429893+Bhatt-Om@users.noreply.github.com>
2023-12-27 16:34:22 +0530

    implemented delete, checked, and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "./App.css"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
@@ -15,6 +16,25 @@ export default function App() {
         },
       ]
     })
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos =>{
+      return currentTodos.map(todo =>{
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos =>{
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   console.log(todos)
@@ -23,17 +43,26 @@ export default function App() {
       <form onSubmit={handleSubmit} className="new-form">
         <label htmlFor="item">New Items</label>
         <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"  />
-        <button className="btn">Add</button>
+        <button className="add-btn">Add</button>
       </form>
       <h1 className="header">To do list</h1>
       <ul className="list">
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="delete-item">Delete</button>
-        </li>
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return(
+            <li key={todo.id}>
+              <label htmlFor="">
+                <input 
+                type="checkbox" 
+                checked={todo.completed}
+                onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button onClick={()=> deleteTodo(todo.id)} className="dle-btn">Delete</button>
+            </li>
+          )
+        })}
       </ul>
     </>
   )
