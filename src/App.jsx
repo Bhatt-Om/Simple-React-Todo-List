5795313ed0c0c4a1bbe6a2f2d642dbcbaf57d2f9
Bhatt-Om <122429893+Bhatt-Om@users.noreply.github.com>
2023-12-27 12:22:27 +0530

    use useState hook to set the value of items and todos on add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
+
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) =>{
+      return[
+        ...currentTodos, 
+        {
+          id: crypto.randomUUID(), title: newItem, completed: false
+        },
+      ]
+    })
+  }
+
+  console.log(todos)
   return (
     <>
-      <form className="new-form">
+      <form onSubmit={handleSubmit} className="new-form">
         <label htmlFor="item">New Items</label>
-        <input type="text" id="item" />
+        <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"  />
+        <button className="btn">Add</button>
       </form>
-      <button className="btn">Add</button>
       <h1 className="header">To do list</h1>
       <ul className="list">
         <li>
